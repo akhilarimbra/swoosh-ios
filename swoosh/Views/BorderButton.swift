0654de4e527637760f307a566e31e0c59a2e3f9e
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-24 00:33:15 +0530

    adding third screen

--- swoosh/Views/BorderButton.swift
+++ swoosh/Views/BorderButton.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class BorderButton: UIButton {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        layer.borderWidth = 3.0
-        layer.borderColor = UIColor.white.cgColor
-        
-    }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    layer.borderWidth = 3.0
+    layer.borderColor = UIColor.white.cgColor
+    
+  }
 }
