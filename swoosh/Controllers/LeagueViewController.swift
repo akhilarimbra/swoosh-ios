df0b453f85d4facfb053c2368011be4b9c436ac0
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-24 01:44:05 +0530

    added seperate view controllers

---
+++ swoosh/Controllers/LeagueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LeagueViewController.swift
+//  swoosh
+//
+//  Created by Akhil Raj Arimbra on 7/24/18.
+//  Copyright © 2018 Akhil Raj Arimbra. All rights reserved.
+//
+
+import UIKit
+
+class LeagueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ swoosh/Controllers/SkillViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SkillViewController.swift
+//  swoosh
+//
+//  Created by Akhil Raj Arimbra on 7/24/18.
+//  Copyright © 2018 Akhil Raj Arimbra. All rights reserved.
+//
+
+import UIKit
+
+class SkillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
